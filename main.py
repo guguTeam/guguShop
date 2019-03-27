a01aaebc4ba7365b4907dad40afa6ce221604bef
soeur <juzi201314@gmail.com>
2019-03-27 15:06:31 +0800

    update

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import database
 global db
 
 if __name__ == '__main__':
+    #初始化数据库
     database.init_database()
+
     nonebot.init(config)
+    #加载list命令
     nonebot.load_plugin('commands.list')
     nonebot.run()
