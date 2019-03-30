b162b8cfcc3d2c9b0208782a6b850275c6afcd5a
SnowXxm <16769334@qq.com>
2019-03-30 17:34:21 +0800

    test

--- commands/user/shop/__init__.py
+++ commands/user/shop/__init__.py
@@ -1,4 +1,5 @@
 from nonebot import on_command, CommandSession
+from nonebot.command.argfilter import extractors
 
 import database
 
@@ -10,14 +11,9 @@ async def item_list(session: CommandSession):
         l += '\n[{}] {} | 库存: {} | ￥{}'.format(i.id, i.name, i.count if (i.count > 0) else '缺货', i.price)
 
     await session.send(l)
-    await session.send('请输入‘购买 [序号] [数量] [宿舍号] [备注(可为空)]’提交购买订单~')
+    #session.get('info', prompt='请输入‘购买 [序号] [数量] [宿舍号] [备注(可为空)]’提交购买订单~'）
+
 
 @item_list.args_parser
 async def _(session: CommandSession):
     pass
-
-
-
-
-
-
